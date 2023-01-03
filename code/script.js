360c0e45a295de6958472e703ab8cd0a0f74f13a
Soufian Tamim <soufiaintamim22@gmail.com>
2023-01-03 16:52:59 +0100

    second commit

--- code/script.js
+++ code/script.js
@@ -1,6 +1,43 @@
-const myModal = document.getElementById("myModal");
-const myInput = document.getElementById("myInput");
+let Random;
 
-myModal.addEventListener("shown.bs.modal", () => {
-  myInput.focus();
-});
+
+function GetRandom() {
+  for (let i = 0 ; i < 6 ;i++) {
+    $.ajax({
+      url: "https://www.themealdb.com/api/json/v1/1/random.php",
+      type: "GET",
+      success: function (data) {
+        Random = data.meals;
+        BuildCard(Random);
+      },
+    });
+  }
+}
+GetRandom()
+
+
+function BuildCard(data){
+  let CardsPlace = document.getElementById("cards");
+  let Card = "";
+
+  for( let i = 0 ; i < 6 ;i++){
+    Card +=`
+    <div class="card" style="width: 18rem; height: 28rem;" id='${data[i].idMeal}'>
+      <img src="${data[i].strMealThumb}" class="card-img-top" alt="Thumbnail ${data[i].strMealThumb}" width="100">
+        <div class="card-body">
+            <h5 class="card-title">${data[i].strMeal}</h5>
+            <p>${data[i].strCategory} ${" : "+ data[i].strArea}</p>
+            
+            <button class="btn btn-primary" id='${data[i].idMeal}' data-bs-toggle="modal" data-bs-target="#exampleModal">learn more ..</button>
+        </div>
+    </div>
+    `
+    CardsPlace.innerHTML += Card
+  }
+}
+
+function DisplayDetails(data){
+
+}
+
+
